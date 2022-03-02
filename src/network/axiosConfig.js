b769f0a2a15046388596355165a3a267d5059d88
AbdelrahmanEhab <57192912+AbdelrahmanEhab20@users.noreply.github.com>
2022-03-02 14:22:28 +0200

    LAst Updates

--- src/network/axiosConfig.js
+++ src/network/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstace = axios.create({
-    baseURL: "http://localhost:3000/api/v1/",
+    baseURL: "http://localhost:8000/api/v1/",
 });
 
 // Add a request interceptor
